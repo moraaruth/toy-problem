4312c41049e9694b2301199b4ba485033c162a46
Ruth Moraa <moraar37@gmail.com>
2022-11-28 18:10:40 +0300

    adding comment for easy understanding

--- grade-generator/index.js
+++ grade-generator/index.js
@@ -1,7 +1,9 @@
+//adding functions, if/else statement to make my app interactive
 function getScore() {
+    // initializing the prompt using prompt method
     let marks1 = prompt("Please enter your score");
 
-
+    // if statement that will ensure the grade is between 0 and 100
     if (marks1 < 0 || marks1 > 100) {
 
         document.getElementById('text').innerHTML = "Hello, Please enter valid marks!";
@@ -9,25 +11,30 @@ function getScore() {
         document.getElementById('grade1').innerHTML = 'null';
 
     }
+    // else if statement to give 'A' grade
 
     else if (marks1 >= 79) {
         document.getElementById('grade1').innerHTML = 'A';
         document.getElementById('text').innerHTML = "Hello, You have grade A";
     }
+    // else if statement to give 'B' grade
     else if (marks1 >= 60 && marks1 < 79) {
         document.getElementById('grade1').innerHTML = 'B';
         document.getElementById('text').innerHTML = "Hello, You have grade B";
     }
+    // else if statement to give 'C' grade
     else if (marks1 >= 49 && marks1 <= 59) {
         document.getElementById('grade1').innerHTML = 'C';
         document.getElementById('text').innerHTML = "Hello, You have grade C";
 
     }
+    // else if statement to give 'D' grade
     else if (marks1 >= 40 && marks1 < 49) {
         document.getElementById('grade1').innerHTML = 'D';
         document.getElementById('text').innerHTML = "Hello, You have grade D";
 
     }
+    // else if statement to give 'E' grade
     else {
         document.getElementById('grade1').innerHTML = 'E';
         document.getElementById('text').innerHTML = "Hello, You have grade E";
@@ -37,3 +44,4 @@ function getScore() {
     document.getElementById('marks1').innerHTML = marks1;
 
 }
+// end of js
