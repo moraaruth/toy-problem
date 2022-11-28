1a06ee0914737a17c67434ed108650394c56a5c4
Ruth Moraa <moraar37@gmail.com>
2022-11-28 10:12:07 +0300

    changing the grade generator to a simpler app

--- grade-generator/index.js
+++ grade-generator/index.js
@@ -1,41 +1,39 @@
 function getScore() {
-    let math = document.getElementById("math").value;
-    let english = document.getElementById("english").value;
-    let kiswahili = document.getElementById("kiswahili").value;
-    let science = document.getElementById("science").value;
-    let arts = document.getElementById("arts").value;
-
-    let total1 = parseFloat(math) + parseFloat(english) + parseFloat(kiswahili) + parseFloat(science) + parseFloat(arts);
-    let marks1 = (total1 * 100) / 500;
+    let marks1 = prompt("Please enter your score");
 
 
+    if (marks1 < 0 || marks1 > 100) {
 
-    if (marks1 >= 79) {
+        document.getElementById('text').innerHTML = "Hello, Please enter valid marks!";
+        document.getElementById('marks1').innerHTML = 'Invalid';
+        document.getElementById('grade1').innerHTML = 'null';
+
+    }
+
+    else if (marks1 >= 79) {
         document.getElementById('grade1').innerHTML = 'A';
+        document.getElementById('text').innerHTML = "Hello, You have grade A";
     }
-    else if (marks1 >= 60 && marks < 79) {
+    else if (marks1 >= 60 && marks1 < 79) {
         document.getElementById('grade1').innerHTML = 'B';
+        document.getElementById('text').innerHTML = "Hello, You have grade B";
     }
-    else if (marks1 >= 49 && marks <= 59) {
+    else if (marks1 >= 49 && marks1 <= 59) {
         document.getElementById('grade1').innerHTML = 'C';
+        document.getElementById('text').innerHTML = "Hello, You have grade C";
 
     }
-    else if (marks1 >= 40 && marks < 49) {
+    else if (marks1 >= 40 && marks1 < 49) {
         document.getElementById('grade1').innerHTML = 'D';
+        document.getElementById('text').innerHTML = "Hello, You have grade D";
 
     }
     else {
         document.getElementById('grade1').innerHTML = 'E';
+        document.getElementById('text').innerHTML = "Hello, You have grade E";
     }
 
-    document.getElementById('total1').innerHTML = total1;
+
     document.getElementById('marks1').innerHTML = marks1;
-
-    if (marks1 < 0 || marks1 > 100) {
-        alert('Please input correct marks');
-        document.getElementById('total1').innerHTML = 'Invalid';
-        document.getElementById('marks1').innerHTML = 'Invalid';
-        document.getElementById('grade1').innerHTML = 'Invalid';
-    }
 
 }
