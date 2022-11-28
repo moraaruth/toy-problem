9a03a9e7cde4f1054952c01c93e3569e44fd6b69
Ruth Moraa <moraar37@gmail.com>
2022-11-28 18:20:20 +0300

    adding comment to net salary calculator

--- net-salary-calculator/index.js
+++ net-salary-calculator/index.js
@@ -1,6 +1,10 @@
+//adding functions, if/else statement to make my app interactive
 let salary = prompt('Hello, please enter your salary + benefits:');
 let netPay = salary - (nssf()) - nssf() - paye();
 
+// writing the nssf, nhif, paye, and calculator functions
+
+// nssf function that shows nssf range sample
 function nssf() {
     if (salary < 100000) {
         return 200;
@@ -12,6 +16,7 @@ function nssf() {
 
 
 }
+// nhif function that shows the nhif range sample
 
 function nhif() {
     if (salary < 100000) {
@@ -26,7 +31,7 @@ function nhif() {
         return 1200;
     }
 }
-
+// paye function that shows how paye will be calculated
 function paye() {
     if (salary < 100000) {
         return (0.2 * salary);
@@ -36,12 +41,14 @@ function paye() {
         return (0.3 * salary);
     }
 }
+// function that will show what output will be given on html
 function salaryCalc() {
     return `salary:${salary};   netpay:${netPay} ; `;
 
 }
+// function that ensures the output has been shown on html 
 const calculateBtn = () => document.getElementById('salaryCalculator').innerHTML = salaryCalc();
 calculateBtn() = text;
 document.getElementById('2salaryCalculator').innerHTML = salaryCalc();
 
-
+// end of js
